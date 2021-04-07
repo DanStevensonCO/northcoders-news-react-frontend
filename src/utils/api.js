54c4db8c1e5c559a5b98d2f9e449e1b4eb090610
DanStevensonCO <danstevensonco@gmail.com>
2021-04-07 11:55:26 +0100

    fix baseURL (capitals) issue

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,29 +1,32 @@
 import axios from 'axios'
 
 const newsApi = axios.create({
-    baseUrl: "https://nc-news-dan-stevenson.herokuapp.com/api"
+    baseURL: "https://nc-news-dan-stevenson.herokuapp.com/api"
 })
 
-const baseUrl = "https://nc-news-dan-stevenson.herokuapp.com/api"
-
 export const getArticles = (topic) => {
-    return newsApi.get((baseUrl + "/articles"), {
-        params: { topic }
-    })
-        .then(({ data }) => {
+    return newsApi.get("/articles").then(({ data }) => {
         return data.articles
     })
 }
 
+
 export const getTopics = () => {
-    return newsApi.get(baseUrl + "/topics").then(({ data }) => {
+    return newsApi.get("/topics").then(({ data }) => {
         return data.topics
     })
 }
 
 export const getArticleById = (article_id) => {
-    return newsApi.get(`${baseUrl}/articles/${article_id}`)
+    return newsApi.get(`/articles/${article_id}`)
         .then(({ data }) => {
             return data.article
+        })
+}
+
+export const getCommentsByArticleId = (article_id) => {
+    return newsApi.get(`/articles/${article_id}/comments`)
+        .then(({ data }) => {
+            return data.comments
         })
 }
